f0d01fb2620a72e49a12de2753df8bae45500e81
Soni <Ankush.Soni@emtecinc.com>
2024-02-18 03:28:00 +0530

    add route links to component

--- crunch-app/src/components/offers.jsx
+++ crunch-app/src/components/offers.jsx
@@ -1,21 +1,26 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import Header from "./header";
+import Box from "@mui/material/Box";
 
 export default function OffersPage() {
   return (
-    <Card
-      sx={{
-        maxWidth: 450,
-        margin: 1,
-        textDecoration: "none",
-        height: "95%",
-        borderRadius: 4,
-      }}
-    >
-      <Typography variant="body2" color="text.secondary">
-        OFFERS PAGE
-      </Typography>
-    </Card>
+    <Box sx={{ flexGrow: 1, height: "100%" }}>
+      <Header />
+      <Card
+        sx={{
+          maxWidth: 450,
+          margin: 1,
+          textDecoration: "none",
+          height: "95%",
+          borderRadius: 4,
+        }}
+      >
+        <Typography variant="body2" color="text.secondary">
+          OFFERS PAGE
+        </Typography>
+      </Card>
+    </Box>
   );
 }
